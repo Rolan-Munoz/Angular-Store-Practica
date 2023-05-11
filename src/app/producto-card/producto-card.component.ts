705a1e8f39b59482305736283e50a97f6ea9702f
Rolan Muñoz <rolanmunoz.dev@gmail.com>
2023-05-11 12:43:11 +0200

    final version

--- src/app/producto-card/producto-card.component.ts
+++ src/app/producto-card/producto-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { Producto } from '../producto.model';
-import { CurrencyPipe } from '@angular/common';
+
 
 
 
@@ -11,22 +11,7 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class ProductoCardComponent {
 
-  constructor(private currencyPipe: CurrencyPipe) {}
 
-  transformarPrecio(price: number): string {
-    const transformedPrice = this.currencyPipe.transform(price, 'EUR', 'symbol', '1.2-2', 'es-ES');
-    return transformedPrice !== null ? transformedPrice : '';
-  }
-  
-  transformarPecioRebajado(discountedPrice: number | string): string {
-    if (typeof discountedPrice === 'number') {
-      const transformedPrice = this.currencyPipe.transform(discountedPrice, 'EUR', 'symbol', '1.2-2', 'es-ES');
-      return transformedPrice !== null ? transformedPrice : '';
-    } else {
-      return '';
-    }
-  }
-  
   
 
   @Input() producto?: Producto;
